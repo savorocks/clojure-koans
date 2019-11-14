211ab624611725833dc0b1bad34fa7652b2bae0e
Savo Djuric <djuricsavo@gmail.com>
2019-11-14 15:29:03 +0100

    work on 16_refs on 14.11.2019

--- src/koans/16_refs.clj
+++ src/koans/16_refs.clj
@@ -17,7 +17,7 @@
           @the-world))
 
   "Alter where you need not replace"
-  (= "hello!!!" (let [exclamator (fn [x] (str x "!"))]
+  (= "better!!!" (let [exclamator (fn [x] (str x "!"))]
           (dosync
            (alter the-world exclamator)
            (alter the-world exclamator)
@@ -25,12 +25,13 @@
           @the-world))
 
   "Don't forget to do your work in a transaction!"
-  (= 0 (do __
+  (= 0 (do
+         (dosync (ref-set the-world 0))
            @the-world))
 
   "Functions passed to alter may depend on the data in the ref"
   (= 20 (do
-          (dosync (alter the-world ___))))
+          (dosync (alter the-world (fn [x] (int 20))))))
 
   "Two worlds are better than one"
   (= ["Real Jerry" "Bizarro Jerry"]
